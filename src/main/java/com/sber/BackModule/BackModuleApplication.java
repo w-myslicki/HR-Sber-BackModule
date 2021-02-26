88098f6be18bfb2f5ef1b5d1a33be7a01ab22bc2
w-myslicki <edwa1d.myslicki@gmail.com>
2021-02-27 01:10:04 +0300

    Connecting to the database commit

---
+++ src/main/java/com/sber/BackModule/BackModuleApplication.java
@@ -0,0 +1,13 @@
+package com.sber.BackModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackModuleApplication.class, args);
+	}
+
+}
